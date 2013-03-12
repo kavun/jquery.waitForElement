22aa31fc2bd953b68ece5d189914d98e356b0134
Kevin Reed <kevin.12389@gmail.com>
2013-03-12 19:21:43 -0400

    fix tests, rename tests

--- tests/jquery.waitForElement.test.js
+++ tests/jquery.waitForElement.test.js
@@ -1,22 +1,21 @@
 
 test( "waitForElement returns a Deferred object", function () {
   	var d = $.waitForElement();
-  	equal( String(d.promise), String($.Deferred().promise().promise));
+  	equal( String(d.promise), String($.Deferred().promise().promise), String(d.promise));
 });
 
-module('async', {
+module('async: passing senarios', {
+
 	setup: function() {
 		window.startTime = +new Date;
 		window.fix = $('#qunit-fixture');
 		window.idToCheck = 'test-element';
-		window.classToCheck = 'test-element';
+		window.classToCheck = 'test-class';
 		window.tagToCheck = 'canvas';
 		window.threshold = 600;
-	},
-	teardown: function() {
-
 	}
-})
+
+});
 
 asyncTest("fails after treshold, if no id is passed in", 1, function() {
 	
@@ -24,7 +23,6 @@ asyncTest("fails after treshold, if no id is passed in", 1, function() {
 		ok(false);
 		start();
 	}).fail(function () {
-
 		ok( ((+new Date) - window.startTime) > threshold - 20 && ((+new Date) - window.startTime) < threshold + 20 );
 		start();
 	});
@@ -54,6 +52,7 @@ asyncTest("finds an element that already exists", 1, function() {
 		ok(false);
 		start();
 	});
+
 });
 
 asyncTest("finds an element that exists after the call but before the threshold", 1, function() {
@@ -69,6 +68,7 @@ asyncTest("finds an element that exists after the call but before the threshold"
 		ok(false);
 		start();
 	});
+
 });
 
 asyncTest("fails if the element exists after the threshold", 1, function() {
@@ -84,9 +84,24 @@ asyncTest("fails if the element exists after the threshold", 1, function() {
 		ok(true);
 		start();
 	});
+
 });
 
-asyncTest("finds an element by class name", 1, function() {
+
+module('async: failure (not implemented) senarios', {
+
+	setup: function() {
+		window.startTime = +new Date;
+		window.fix = $('#qunit-fixture');
+		window.idToCheck = 'test-element';
+		window.classToCheck = 'test-class';
+		window.tagToCheck = 'canvas';
+		window.threshold = 600;
+	}
+
+});
+
+asyncTest("fails finding an element by class name", 1, function() {
 
 	setTimeout(function () { 
 		fix.append('<div class="' + classToCheck + '"></div>'); 
@@ -99,19 +114,21 @@ asyncTest("finds an element by class name", 1, function() {
 		ok(true);
 		start();
 	});
+
 });
 
-asyncTest("finds an element by tag name", 1, function() {
+asyncTest("fails finding an element by tag name", 1, function() {
 
 	setTimeout(function () { 
 		fix.append('<' + tagToCheck + '></' + tagToCheck + '>'); 
 	}, threshold - 200);
 
-	$.waitForElement(classToCheck, threshold).done(function () {
+	$.waitForElement(tagToCheck, threshold).done(function () {
 		ok(false);
 		start();
 	}).fail(function () {
 		ok(true);
 		start();
 	});
+	
 });
